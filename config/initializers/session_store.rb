17cdc28cd523c4112979502f1fdc0a7a8cc0a883
simon <simon@doggettsfarm.com>
2021-05-09 13:16:30 +0100

    Remove sessions
    We don't need them

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+Rails.application.config.session_store :disabled
+
+# Because we don't use sessions we don't need a secret key base. Pick a random
+# one to satisfy Rails. If we ever were to start using sessions the key would
+# need to be made static.
+Rails.application.config.secret_key_base = SecureRandom.hex
